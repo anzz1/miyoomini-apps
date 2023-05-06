2c2e8d6786d1d07027c1a1fb8a35e4cec27e2cc3
anzz1 <anzz1@live.com>
2023-05-06 10:39:59 +0300

    Add files via upload

---
+++ oss/src/oss.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <unistd.h>
+
+/*
+ * Audioserver input pipe in: /tmp/audio_fifo_server
+ * Audioserver ioctl pipe in: /tmp/audio_ioctl_req
+ * Audioserver ioctl pipe out: /tmp/audio_ioctl_res
+ *
+ * Bypassing the libpadsp mutex locks like this will probably most likely
+ * definitely maybe mess with anything else using the audioserver concurrently
+ *
+ * Default format: raw pcm, signed 16-bit, little endian, stereo, 44100hz (pcm_s16le)
+ *
+ * ffmpeg -y -i in.wav -f s16le -acodec pcm_s16le -ar 44100 -ac 2 out.raw
+ *
+ */
+
+int main(int argc, char *argv[]) {
+  FILE* fp;
+  if (argc > 1) {
+    fp = fopen(argv[1], "rb");
+  } else {
+    freopen(NULL, "rb", stdin);
+    fp = stdin;
+  }
+  if (fp) {
+    int fd = open("/tmp/audio_fifo_server", O_RDWR, 438);
+    if (fd != -1) {
+      int16_t s;
+      while (fread(&s, sizeof(s), 1, fp) == 1) write(fd, &s, sizeof(s));
+      close(fd);
+    }
+    if (fp != stdin) fclose(fp);
+  }
+  return 0;
+}
